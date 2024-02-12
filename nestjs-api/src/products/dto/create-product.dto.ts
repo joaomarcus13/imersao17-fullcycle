63bf099626e94938ebc162234ba64b4a2174712b
joaomarcus13 <joaomarcus13@hotmail.com>
2024-02-12 19:54:58 -0300

    commit

---
+++ nestjs-api/src/products/dto/create-product.dto.ts
@@ -0,0 +1,10 @@
+export class CreateProductDto {
+    name: string;
+  
+    description: string;
+  
+    image_url: string;
+  
+    price: number;
+  }
+  
---
+++ nextjs-frontend/src/theme.ts
@@ -0,0 +1,24 @@
+'use client';
+
+import { Roboto } from 'next/font/google';
+import { createTheme } from '@mui/material/styles';
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+    background: {
+      default: '#8a8a8a',
+    },
+  },
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
+});
+
+export default theme;
